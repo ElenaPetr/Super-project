8b2c03ae4d132e55583319fb88954db7ee681a59
Elena <olpetrushova@gmail>
2017-10-24 23:56:44 +0300

    added card component, modified app.component and index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'my-app',
+  templateUrl:`
+    <nav>
+      <a routerLink="/card" routerLinkActive="active">Media</a>
+    </nav>
+    <router-outlet></router-outlet>
+  ` 
+  
 })
-export class AppComponent {
-  title = 'app';
-}
+export class AppComponent {}
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,43 @@
+import { Component,OnInit }	from '@angular/core';
+import { Router }					from '@angular/router';
+import { UUID }						from 'angular2-uuid';
+
+import { Media }						from './media';
+import { MediaService }			from './media.service';
+ 
+@Component({
+  selector: 'my-media',
+  templateUrl: './media.component.html',
+})
+export class MediaComponent implements OnInit{
+	medi:Media[];
+	selectedPhoto:Media;
+	
+	constructor(private router:Router, private mediaService: MediaService) { }
+	
+	ngOnInit(): void {
+		this.getMedia();
+	}
+
+	getMedia(): void {
+		this.mediaService.getMedias().then(response => this.medi = response);
+	}
+	
+	delete(media: Media): void {
+		this.mediaService
+			.delete(media.id).then(() => {
+				this.medi = this.medi.filter(h => h !== media);
+				if (this.selectedPhoto === media) { 
+					this.selectedPhoto = null;
+				}});
+	}
+
+	onSelect(photo:Media) : void {
+		this.selectedPhoto=photo;
+		this.gotoDetail();
+	}
+	
+	gotoDetail(): void {
+		this.router.navigate(['/update', this.selectedPhoto.id]);
+	}
+}
